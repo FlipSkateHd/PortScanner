b01e1714b7b0ddb8c86a5d4cbe31010933903caf
flipskatehd <flipskatehd@gmail.com>
2025-05-28 17:10:24 -0300

    Primeiro Commit :)

---
+++ Ping.py
@@ -0,0 +1,17 @@
+import socket
+import psutil
+
+todasConexões = psutil.net_connections(kind='inet')
+
+
+primeira_conexao = todasConexões[0]
+primeiroIp = primeira_conexao.raddr.ip
+protocoloPrimeiroIp = primeira_conexao.type
+
+if protocoloPrimeiroIp == 1:
+    protocoloPrimeiroIp = "TCP"
+else:
+    protocoloPrimeiroIp = "UDP"
+
+print(f"Primeiro IP das conexões: {primeiroIp}")
+print(f"Protocolo do primeiro IP: { protocoloPrimeiroIp}")
