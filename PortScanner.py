0c08bd3e4b2b6131d78b4d619f5ed1cdb8ab0a7f
flipskatehd <flipskatehd@gmail.com>
2025-06-10 18:14:17 -0300

    Função auto adicionada no portscanner

--- PortScanner.py
+++ PortScanner.py
@@ -1,19 +1,43 @@
 import socket
 
-socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria um socket TCP
-socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria um socket UDP
+def manualPorts():
+    socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria um socket TCP
+    socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Cria um socket UDP
+    domain = input("Insert your domain: ")
+    ports = input("Insert your ports with an comma (default: 80, 443): ")
+    ports = ports.split(",")
 
-dominio = input("Digite o dominio: ")
-portas = input("Digite as portas separadas por virgula (ex: 80, 443): ")
-portas = portas.split(",")
-
-portas = [int(porta) for porta in portas]
-
-
-for porta in portas:
-    if socketTCP.connect_ex((dominio, porta)) == 0:  # connect_ex() retorna 0 se o socket conectou
-        print(f"Conectado ao servidor na porta {porta}")
+    if ports[0] == "":
+        ports = [80, 443]    
     else:
-        print(f"Nao foi possivel conectar ao servidor na porta {porta}")
+        ports = [int(port) for port in ports]
 
-socketTCP.close()
+
+    for port in ports:
+        if socketTCP.connect_ex((domain, port)) == 0:  # connect_ex() retorna 0 se o socket conectou
+            print(f"Open port:/ {port}")
+        else:
+            print(f"Closed port:/ {port}")
+
+        socketTCP.close()
+
+
+
+def fullScan():
+    LHOST = input("Insert host: ")
+    for PORTS in range (1,6535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if s.connect_ex((LHOST, PORTS)) == 0:
+            print(f"Open port:/ {PORTS}")
+        else:
+            pass
+    
+
+def main():
+    mode = input("Insert 1 for manual ports or 2 for full scan: ")
+    if mode == "1":
+        manualPorts()
+    else:
+        fullScan()
+
+main()
